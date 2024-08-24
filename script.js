664821e5a34ed1296b049d854133cc7700e6be15
Aniket Jadhav <aniket.jadhav@travelandpayments.com>
2024-08-24 15:43:41 +0530

    Fixed the 'What We Offer' text getting overlapped by the navbar

--- script.js
+++ script.js
@@ -1,61 +1,61 @@
-const navMenu = document.querySelector(".nav-menu");
-const navClose = document.querySelector(".nav-close");
-const navLinks = document.querySelector(".navlinks");
-const overlay = document.querySelector(".overlay");
+const navMenu = document.querySelector(".nav-menu")
+const navClose = document.querySelector(".nav-close")
+const navLinks = document.querySelector(".navlinks")
+const overlay = document.querySelector(".overlay")
 
 navMenu.addEventListener("click", () => {
-  navLinks.classList.add("nav-open");
-  overlay.classList.add("show");
-});
+  navLinks.classList.add("nav-open")
+  overlay.classList.add("show")
+})
 
 navClose.addEventListener("click", () => {
-  navLinks.classList.remove("nav-open");
-  overlay.classList.remove("show");
-});
+  navLinks.classList.remove("nav-open")
+  overlay.classList.remove("show")
+})
 
 overlay.addEventListener("click", () => {
-  navLinks.classList.remove("nav-open");
-  overlay.classList.remove("show");
-});
+  navLinks.classList.remove("nav-open")
+  overlay.classList.remove("show")
+})
 
+gsap.to(".card-up", {
+  yPercent: -100,
+  stagger: 0.5,
+  scrollTrigger: {
+    trigger: ".service-content",
+    start: () => `top ${document.querySelector("nav").offsetHeight + 16}`,
+    end: "+1500px",
+    scrub: 0.5,
+    pin: ".service-section",
+    // markers: true,
+  },
+})
 
-const mm = gsap.matchMedia();
-console.log(mm)
+const mm = gsap.matchMedia()
 
 mm.add(
   {
-  isTabletPortraitUp: "(min-width: 641px)",
-  isTabletLandscapeDown: "(max-width: 1024px)",
-}, (context) => {
-  let {isTabletPortraitUp, isTabletLandscapeDown} = context.conditions
-  
-  gsap.to('.card-up', {
-    yPercent: -100,
-    stagger: 0.5,
-    scrollTrigger: {
-      trigger: '.service-section',
-      start: isTabletLandscapeDown ? 'top 5%' : 'top 10%',
-      end: '+1500px',
-      scrub: .5,
-      pin: true
+    isTabletPortraitUp: "(min-width: 641px)",
+  },
+  (context) => {
+    let { isTabletPortraitUp } = context.conditions
+
+    if (isTabletPortraitUp) {
+      gsap.to(".hero-img--horizontal", {
+        xPercent: -20,
+        yoyo: true,
+        repeat: -1,
+        duration: 2.25,
+        ease: "power1.inOut",
+      })
+    } else {
+      gsap.to(".hero-img--vertical", {
+        yPercent: 10,
+        yoyo: true,
+        repeat: -1,
+        duration: 2.25,
+        ease: "power1.inOut",
+      })
     }
-  })
-  
-  if (isTabletPortraitUp) {
-    gsap.to(".hero-img--horizontal", {
-      xPercent: -20,
-      yoyo: true,
-      repeat: -1,
-      duration: 2.25,
-      ease: "power1.inOut",
-    });
-  } else {
-    gsap.to(".hero-img--vertical", {
-      yPercent: 10,
-      yoyo: true,
-      repeat: -1,
-      duration: 2.25,
-      ease: "power1.inOut",
-    });
   }
-});
+)
